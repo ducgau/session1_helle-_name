ae48e9fef8502020ee045d9560c7fd2c2b325261
admin <ducgau99@gmail.com>
2019-03-28 22:18:59 +0800

    finish

---
+++ src/main/java/com/codegym/hello.java
@@ -0,0 +1,13 @@
+package com.codegym;
+
+import java.util.Scanner;
+
+public class hello {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the name: ");
+        String name = sc.nextLine();
+
+        System.out.println("hello: " + name);
+    }
+}
